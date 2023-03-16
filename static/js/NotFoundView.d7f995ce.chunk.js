baaf1320b20af82a743b69e0fd50b08ec6d3bb26
Oleg-Rudenko <51856760+Oleg-Rudenko@users.noreply.github.com>
2023-03-16 19:31:28 +0000

    Deploying to gh-pages from @ Oleg-Rudenko/goit-react-hw-05-movies@4c343010510acb0ee651c49ed53e0f5080444ec4 🚀

---
+++ static/js/NotFoundView.d7f995ce.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[558],{4126:function(e,t,n){n.r(t),n.d(t,{default:function(){return u}});var r=n(184);function u(){return(0,r.jsx)("h1",{children:"404 - page not found"})}}}]);
+//# sourceMappingURL=NotFoundView.d7f995ce.chunk.js.map
